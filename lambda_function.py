3f9f549c089c2864e8b76c5a0618cea2d277f26e
Victor Paulo <victorpaulo.ti@gmail.com>
2024-04-27 18:34:04 -0300

    feat: change workspace terraform

--- lambda_function.py
+++ lambda_function.py
@@ -6,5 +6,5 @@ print('Loading function')
 def lambda_handler(event, context):
 
     hello = 'HELLO WORLD!!'
-    error = 'erro encontrado no servico'
+    #error = 'erro encontrado no servico'
     return hello
