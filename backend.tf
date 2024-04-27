8bd2537177b2f73fcf0b0cfb95c24f64da8f033e
Victor Paulo <victorpaulo.ti@gmail.com>
2024-04-27 11:32:44 -0300

    feat: new workspace terraform

--- backend.tf
+++ backend.tf
@@ -5,7 +5,7 @@ terraform {
 
     # The name of the Terraform Cloud workspace to store Terraform state files in.
     workspaces {
-      name = "VPAULO_COM_MEI"
+      name = "terraform"
     }
   }
 }
